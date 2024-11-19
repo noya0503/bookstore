76b801490b861d5c37b73f398c764a8618b730fd
noya0503 <1109302030@gap.wzu.edu.tw>
2024-11-19 17:49:39 +0800

    new

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from book.models import book
+
+class bookAdmin(admin.ModelAdmin):
+    list_display = ('bookname', 'author', 'publisher', 'pubdate','price')
+    list_filter = ('author', 'publisher')
+    search_fields = (['bookname'])
+    ordering = (['pubdate'])
+# 將資料表註冊到後台管理
+admin.site.register(book, bookAdmin)
